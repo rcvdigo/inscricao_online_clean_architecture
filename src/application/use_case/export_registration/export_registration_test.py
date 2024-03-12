c6a6c38e11ff8f7ad9c9fdc9d6edfa1c841d9490
rcvdigo <rcvdigo@gmail.com>
2024-03-12 01:43:43 -0300

    fix: corrigindo chamadas de dependencias no arquivo export_registration_test.py

--- src/application/use_case/export_registration/export_registration_test.py
+++ src/application/use_case/export_registration/export_registration_test.py
@@ -1,40 +1,19 @@
 import os
+import mysql.connector
 from pathlib import Path
-from datetime import date
 from .export_registration import ExportRegistration
-from src.application.use_case.export_registration.input_boundary import InputBoundary
-from src.domain.repositories.load_registration_repository import LoadRegistrationRepository
-from src.application.contracts.export_registration_pdf_exporter import PdfExporter
-from src.application.contracts.storage import Storage
-from src.domain.value_objects.cpf import Cpf
-from src.domain.entities.registration import Registration
+from .input_boundary import InputBoundary
+from src.infra.repositories.mysql.pdo_registration_repository import PdoRegistrationRepository
 
-
-# class MockUseCase(LoadRegistrationRepository):
-    
-#     def __init__(self) -> None:
-#         pass
-
-#     def load_registration_number(self, cpf: Cpf) -> Registration:
-#         pass
 
 def test():
 
     path = Path(os.path.abspath('src'))
 
-    # Entities
-    registration = Registration(
-        name='Rodrigo',
-        email='rcvdigo@gmail.com',
-        birth_date=date.today(),
-        registration_number='40926288890',
-        registration_at=date.today()
-    )
-
     # Use Cases
-    load_registration_repository = LoadRegistrationRepository()
-    pdf_exporter = PdfExporter()
-    storage = Storage()
+    load_registration_repository = PdoRegistrationRepository(pdo=mysql.connector)
+    # pdf_exporter = ExportRegistrationPdfExporter()
+    # storage = Storage()
 
     export_registration_use_case = ExportRegistration(
         repository=load_registration_repository,
