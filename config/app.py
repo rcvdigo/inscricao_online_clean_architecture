10175e7407640b7553d5fcd2a1598349516a026b
rcvdigo <rcvdigo@gmail.com>
2024-03-12 01:45:58 -0300

    feat: Adicionando arquivo app.py, de configuração do banco de dados

---
+++ config/app.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+
+def config() -> Dict[str, Dict[str, str]]:
+    return {
+        'db': {
+            'host': 'localhost',
+            'user': 'root',
+            'password': 'root',
+            'database': 'clean_database'
+        }
+    }
